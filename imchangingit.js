6116d43a31f9675ed9999f2493f8d9507bca3ddc
Helana Maliakal <maliakaa@tcd.ie>
2019-11-14 18:55:50 +0000

    trying to get a search bar working

--- imchangingit.js
+++ imchangingit.js
@@ -1,8 +1,29 @@
-const Http = new XMLHttpRequest();
-const url='https://api.github.com/users/octocat/orgs';
-Http.open("GET", url);
-Http.send();
+const searchBtn = document.getElementById('search-btn');
+const search = document.getElementById('username');
+const tip = document.getElementById('tip');
 
-Http.onreadystatechange = (e) => {
-  console.log(Http.responseText)
-}
+searchBtn.addEventListener('click', () =>  {
+  search.style.width ='80%';
+  search.style.paddingLeft ='80px';
+  search.style.cursor='text';
+  search.focus();
+  
+  var i = 0;
+  var message = 'Type in a username e.g hellebelle';
+  var speed = 100;
+  
+  function typeWriter(){
+    if(i<message.length){
+      msg = search.getAttribute('placeholder') + message.charAt(i);
+      search.setAttribute('placeholder', msg)
+      i++;
+      setTimeout(typeWriter, speed);
+    }
+  }
+  typeWriter();
+})
+
+search.addEventListener('keydown', () => {
+  tip.style.visibility = 'visible';
+  tip.style.opacity = '1';
+})
