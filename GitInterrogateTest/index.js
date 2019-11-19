9434d2b02eb4047305f7b445840757a04691cbc4
Helana Maliakal <maliakaa@tcd.ie>
2019-11-19 16:48:55 +0000

    loading data

--- GitInterrogateTest/index.js
+++ GitInterrogateTest/index.js
@@ -1,6 +1,8 @@
-import { callbackify } from "util";
 
 $(document).ready(function(){
+    const client_id = "Iv1.e4951da8a6463f1a";
+    const client_secret = "76b3aeea23ceb4702e2810ff301d8effc65fda2e";
+    
     $("#search-bar").click(function() {
        $("#search").css("width","80%"); 
        $("#search").css("paddingLeft","60px");
@@ -31,22 +33,62 @@ $(document).ready(function(){
         $("#btn").css("visibility","hidden");
 
         var user = $("#search").val() ? $("#search").val() : "github";
-        loadData(user, displayUser);
+        loadData(user, displayUser, displayRepos);
 
-        function loadData(user,callback1,callback2) {
+        async function loadData(user,callback1,callback2) {
             
-            var url_User = "https://api.github.com/users/" + user;
-            console.log(url_User);
-            $.get( url_User, function (data, status) {
+            //var url_User = "https://api.github.com/users/" + user;
+            const response = await fetch(`https://api.github.com/users/${user}?client_id=${client_id}&
+            client_secret=${client_secret}`);
+            const result = await response.json();
+            console.log(result);
+/*             $.get( url_User, function (data, status) {
                 console.log(status);
                 success: callback1(data, status);
-            });
-            var url_Repos = "https://api.github.com/users/" + searchterm + "/repos"
-            $.get(url_Repos,function(data, status){
+            }); */
+           // var url_Repos = ;
+            const repos_response = await fetch(result.repos_url + `?client_id=${client_id}&
+            client_secret=${client_secret}`);
+            const repos_result = await repos_response.json();
+            console.log(repos_result);
+            
+            //number of commits
+            var commits = [];
+            for(var i = 0; i< repos_result.length; i++){
+                var obj = repos_result[i];
+                const contributors = await fetch(obj.contibutors_url + `?client_id=${client_id}&
+                client_secret=${client_secret}`);
+                const con_results = await contributors.json();
+                console.log(con_results);
+
+            }
+           /*  repos_result.items.forEach(i => {
+                var repo = new Object();
+                repo.key = i.name;
+                const contributors = i.contributors_url;
+                var commits = 0
+                contributors.items.forEach(j => {
+                   commits+=j.contributions;
+                   console.log(commits);
+                });
+                repo.value = commits;  
+            }); */
+            //"https://api.github.com/users/" + user + "/repos"
+/*             $.get(url_Repos,function(data, status){
                 console.log(status);
                 success: callback2(data,status);
-            });
+            }); */
         };
+
+        function displayUser(data, status) {
+            $("div.container").remove();
+            $("#username").append(data.login);
+        }
+
+        function displayRepos(data,status) {
+            
+        }
+
     })
     
 
